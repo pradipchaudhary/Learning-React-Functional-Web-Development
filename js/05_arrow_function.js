beda1962f95e39d6b10b54f48102c41037508fa4
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-03-24 17:47:30 +0545

    Fix this, using regullar function

--- js/05_arrow_function.js
+++ js/05_arrow_function.js
@@ -1,20 +1,37 @@
-console.log("Arrow Function;");
+// Method one
 
-// Normal function
+// const dharan = {
+// 	resorts: ["Hotel Inn", "Namaste Hotel", "Ratna hotel"],
+// 	print: function (delay = 2000) {
+// 		setTimeout(function () {
+// 			console.log(this);
+// 		}, delay);
+// 	},
+// };
+// dharan.print();
 
-function add(a, b) {
-	return a + b;
-}
+// method 2
+// const biratnagar = {
+// 	schools: ["school1", "school2", "school3", "school4"],
+// 	print: (delay = 1000) => {
+// 		setTimeout(() => {
+// 			console.log(this); // this is windows object
+// 			console.log(this.schools.join(","));
+// 		});
+// 	},
+// };
 
-console.log(add(2, 4));
+// biratnagar.print();
 
-// Arrow function
-const addme = (a, b) => {
-	console.log(a + b);
+//
+
+const schoolList = {
+	schools: ["Ramjank", "Satayadebi", "evergreen", "Merryland"],
+	print: function (delay = 2000) {
+		setTimeout(() => {
+			console.log(this.schools.join(","));
+		}, delay);
+	},
 };
-addme(1, 2);
 
-const findRoot = (a) => {
-	return a * a;
-};
-console.log(findRoot(4));
+schoolList.print();

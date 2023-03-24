a0069ff5c1e5caf937a061ef9e79b2b55240d218
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-03-24 10:38:58 +0545

    Variable.....

--- js/02_variables.js
+++ js/02_variables.js
@@ -1,3 +1,27 @@
+// Var
+
 var piza = true;
 piza = false;
 console.log(piza);
+
+// const Variables
+// const pizza = true;
+// pizza = false;
+// console.log(pizza);
+
+var topic = "JavaScript";
+if (topic) {
+	var topic = "React";
+	console.log("inner : " + topic);
+}
+console.log("Outer : " + topic);
+
+let message = "Global variables";
+if (message) {
+	let message = "Block Scope Variable";
+	console.log(message);
+}
+console.log(message);
+
+var div,
+	container = document.getElementById();

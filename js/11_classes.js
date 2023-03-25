7cabe620615c02a52c5a17348ec7d744805b5ea5
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-03-25 19:42:47 +0545

    Create Class with Class keyword...(ES6 Method)

--- js/11_classes.js
+++ js/11_classes.js
@@ -1,20 +1,33 @@
 // Classes
-console.log("Classes");
+// console.log("Classes");
 
-function Vacation(destination, length) {
-	this.destination = destination;
-	this.length = length;
+// function Vacation(destination, length) {
+// 	this.destination = destination;
+// 	this.length = length;
+// }
+
+// Vacation.prototype.print = function () {
+// 	console.log(`${this.destination} | ${this.length} days.`);
+// };
+
+// Vacation.prototype.welcomeMessage = function () {
+// 	console.log(`Welcome to the ${this.destination}. for ${this.length} days.`);
+// };
+
+// const pradip = new Vacation("Pokhara", 7);
+
+// pradip.print();
+// pradip.welcomeMessage();
+
+class Vacation {
+	constructor(destination, length) {
+		this.destination = destination;
+		this.length = length;
+	}
+	print() {
+		console.log(`${this.destination} will take ${this.length} deys.`);
+	}
 }
 
-Vacation.prototype.print = function () {
-	console.log(`${this.destination} | ${this.length} days.`);
-};
-
-Vacation.prototype.welcomeMessage = function () {
-	console.log(`Welcome to the ${this.destination}. for ${this.length} days.`);
-};
-
-const pradip = new Vacation("Pokhara", 7);
-
-pradip.print();
-pradip.welcomeMessage();
+const trip = new Vacation("Pradip Chaudhary", 7);
+trip.print();
